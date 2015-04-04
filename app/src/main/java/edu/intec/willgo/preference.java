4b7c1c7a1d579c0083cacd7adb771dc0bf9be32e
luisd25 <luis-dominguez1994@hotmail.com>
2015-04-04 01:45:39 -0400

    add preference class

---
+++ app/src/main/java/edu/intec/willgo/preference.java
@@ -0,0 +1,17 @@
+package edu.intec.willgo;
+
+/**
+ * Created by LUIS on 4/3/2015.
+ */
+public class preference {
+    public String name;
+    public String place;
+    public String coor;
+
+    public preference(String name, String place, String coor){
+        this.name=name;
+        this.place=place;
+        this.coor=coor;
+    }
+
+}
